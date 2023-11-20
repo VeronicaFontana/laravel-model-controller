06790ae34adf8fccfb414d30b45f593baa4cfee2
VeronicaFontana <veronyc98@hotmail.it>
2023-11-20 23:51:43 +0100

    [ADD] Sass

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,17 @@
 
 @section("content")
 
-    <h1>Home</h1>
+    <h1>Movies</h1>
     <div class="container d-flex flex-wrap">
         @foreach ($movies as $movie)
             <div class="card m-3" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">{{ $movie->title }}</h5>
-                    <p class="card-text">{{ $movie->nationality }}</p>
-                    <p class="card-text">{{ $movie->vote }}</p>
+                    <p class="card-text">Voto: {{ $movie->vote }}</p>
                     <a href="{{ route("details", ["id" => $movie->id]) }}" class="btn btn-primary">Info</a>
                 </div>
             </div>
         @endforeach
-
     </div>
 
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,13 +2,7 @@
 
     <ul class="nav">
         <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Movies</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Details</a>
+            <a class="nav-link" aria-current="page" href="{{ route("home") }}">Home</a>
         </li>
     </ul>
 
