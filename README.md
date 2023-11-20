94e917be889b86a8f5c802562dfeeb972570c386
VeronicaFontana <veronyc98@hotmail.it>
2023-11-20 22:55:21 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Laravel Model Controller
+
+Oggi facciamo la nostra prima vera interazione con il database utilizzando l’ORM di Laravel.
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Movies</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Details</a>
+        </li>
+    </ul>
+
+</header>
