743a52104c5515f0fc51be1a300998134779cc1b
VeronicaFontana <veronyc98@hotmail.it>
2023-11-20 23:23:00 +0100

    [ADD] Card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,18 @@
 @section("content")
 
     <h1>Home</h1>
-    <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa blanditiis inventore dolorum facilis provident distinctio, labore sapiente suscipit nemo, nisi corrupti quis tempore ea odio, sit reiciendis est aperiam aut! Corrupti a porro incidunt recusandae perspiciatis deleniti alias nemo, cupiditate totam tempore reprehenderit quia adipisci dolore eligendi, aperiam, tenetur quaerat praesentium fugit molestiae ratione fuga veniam numquam doloremque. Quisquam, aperiam voluptates quae doloribus praesentium cumque optio soluta cum odio? Iure doloribus tenetur totam corporis cumque, at vitae. Aliquid ullam inventore, architecto neque dicta accusantium. Porro nihil temporibus explicabo illum molestias laboriosam, ducimus unde qui consequatur error atque! Obcaecati, ex ad?
-    </p>
+    <div class="container d-flex flex-wrap">
+        @foreach ($movies as $movie)
+            <div class="card m-3" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $movie->title }}</h5>
+                    <p class="card-text">{{ $movie->nationality }}</p>
+                    <p class="card-text">{{ $movie->vote }}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        @endforeach
+
+    </div>
 
 @endsection
